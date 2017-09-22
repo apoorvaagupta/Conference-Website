4e9322179492d67097becf45280ad2f84966ba80
apoorvaagupta <apoorvaagupta01@gmail.com>
2017-09-22 23:45:06 +0530

    home page chnges
    Signed-off-by: apoorvaagupta <apoorvaagupta01@gmail.com>

--- index.js
+++ index.js
@@ -15,27 +15,25 @@ $(document).ready(function () {
 
   $('#homeButton').click(function () {
     mainWrapper.empty();
-    mainWrapper.append(`
-                    <div style="background-color: white;">
-
+    mainWrapper.append(`<div style="background-color: white; ">
                 <div class="row">
                     <div class="col-md-12" style="padding: 30px;">
-                        <h1 style="font-family: 'Roboto Condensed', sans-serif;color: #EA4335; border-bottom: solid 1px #bbb; line-height: 1.2;margin-bottom: 15px;font-size: 36px;text-transform: uppercase;">
+                        <h1 style="font-family: 'Roboto Condensed', sans-serif;color: #EA4335; border-bottom: solid 1px #bbb;line-height: 1.2;margin-bottom: 15px;font-size: 36px;text-transform: uppercase;">
                             Overview</h1>
 
                         <div class="overview-description" style="text-align: center;">
                             &nbsp; &nbsp; &nbsp; &nbsp; <span
-                                style="background-color: white; color: black; font-size: 14pt;">Welcome to the &nbsp;</span><br><b
+                                style="background-color: white; color: black; font-size: 14pt;"><img src="images/springer-logo.jpg" height="50px">&nbsp;&nbsp;Welcome to the &nbsp;</span><br><b
                                 style="background-color: white; color: black; font-size: 14pt;"><span
-                                style="color: #385623; font-size: 15pt;">2018 International Conference <br> Towards Extensible and Adaptable Methods in Computing (TEAMC 2018) </span></b>
-                            <b style="background-color: white; color: black; font-size: 14pt;"></b><span
-                                style="background-color: white; color: black; font-size: 14pt;">&nbsp; <br> organized by&nbsp;</span>
+                                style="color: #385623; font-size: 15pt;">2018 International Conference <br> Towards Extensible and Adaptable Methods in Computing <br> TEAMC 2018 </span></b>
+                            <!--<b style="background-color: white; color: black; font-size: 14pt;"></b><span-->
+                                <!--style="background-color: white; color: black; font-size: 14pt;">&nbsp; <br> organized by&nbsp;</span>-->
                         </div>
 
 
                         <div style="background: white; line-height: 18pt; margin: 0in; text-align: center;">
                             <div style="text-align: center;">
-                                </div>
+                            </div>
                             <div style="text-align: center;">
                                 <b><span style="color: #002060; font-size: 15pt;">Netaji Subhas Institute of Technology, Dwarka, Delhi, India.</span></b>
                             </div>
@@ -56,19 +54,19 @@ Extensible and adaptable computing refers to the array of methods and techniques
                         <div class="home-list">
                             <ul class="list-group">
                                 <li class="list-group-item">
-                                    Agile Software Development
+                                    <b style="color: #002060;">Agile Software Development</b>
                                 </li>
                                 <li class="list-group-item">
-                                    Data Management
+                                    <b style="color: #002060;">Data Management</b>
                                 </li>
                                 <li class="list-group-item">
-                                    Web Intelligence
+                                    <b style="color: #002060;">Web Intelligence</b>
                                 </li>
                                 <li class="list-group-item">
-                                    Computing for Education
+                                    <b style="color: #002060;">Computing for Education</b>
                                 </li>
                                 <li class="list-group-item">
-                                    Machine Learning
+                                    <b style="color: #002060;">Machine Learning</b>
                                 </li>
                             </ul>
                         </div>
@@ -77,7 +75,7 @@ Extensible and adaptable computing refers to the array of methods and techniques
                              style="background: white; line-height: 18pt; margin: 0; text-align: justify; text-indent: 0.5in;">
                             <span style="color: black; font-size: 14pt;"><br></span>
                             <span style="color: black; font-size: 14pt;">
-                            The conference will feature regular technical sessions, keynote speeches, workshops, panel discussions and a design contest.
+                            The conference will feature regular technical sessions, keynote speeches, workshops, panel discussions and a Hackathon/Design contest.
                             </span>
                         </div>
 
@@ -85,9 +83,11 @@ Extensible and adaptable computing refers to the array of methods and techniques
                 </div>
                 <div class="row">
                     <div class="col-sm-12 col-md-12" style="padding: 30px;">
-                        <div id="carouselExampleIndicators" style="width: 100%;" class="carousel slide" data-ride="carousel" data-interval="2000">
+                        <div id="carouselExampleIndicators" style="width: 100%;" class="carousel slide"
+                             data-ride="carousel" data-interval="2000">
                             <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0"
+                                    class="active"></li>
                                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                                 <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
@@ -109,54 +109,71 @@ Extensible and adaptable computing refers to the array of methods and techniques
                                     <img class="d-block img-fluid" src="./images/5.jpg" alt="First slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/_MG_9209.JPG" alt="Second slide">
+                                    <img class="d-block img-fluid" src="./images/_MG_9209.JPG"
+                                         alt="Second slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/_MG_9377.JPG" alt="Third slide">
+                                    <img class="d-block img-fluid" src="./images/_MG_9377.JPG"
+                                         alt="Third slide">
                                 </div>
 
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/DSC06193.JPG" alt="Fourth slide">
+                                    <img class="d-block img-fluid" src="./images/DSC06193.JPG"
+                                         alt="Fourth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/DSC_0408.jpg" alt="Fifth slide">
+                                    <img class="d-block img-fluid" src="./images/DSC_0408.jpg"
+                                         alt="Fifth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/DSC_0441.jpg" alt="Sixth slide">
+                                    <img class="d-block img-fluid" src="./images/DSC_0441.jpg"
+                                         alt="Sixth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/DSC_6735.jpg" alt="Seventh slide">
+                                    <img class="d-block img-fluid" src="./images/DSC_6735.jpg"
+                                         alt="Seventh slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/IMG_20150826_222421.jpg" alt="Eighth slide">
+                                    <img class="d-block img-fluid" src="./images/IMG_20150826_222421.jpg"
+                                         alt="Eighth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/india%20gate%20with%20chatri.jpg" alt="Ninth slide">
+                                    <img class="d-block img-fluid"
+                                         src="./images/india%20gate%20with%20chatri.jpg" alt="Ninth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/indiagate.jpg" alt="Tenth slide">
+                                    <img class="d-block img-fluid" src="./images/indiagate.jpg"
+                                         alt="Tenth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/jantar%20mantar.png" alt="Eleventh slide">
+                                    <img class="d-block img-fluid" src="./images/jantar%20mantar.png"
+                                         alt="Eleventh slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/lotus%20temple%20.jpg" alt="Twelveth slide">
+                                    <img class="d-block img-fluid" src="./images/lotus%20temple%20.jpg"
+                                         alt="Twelveth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/qutub%20minar%20slice%20of%20history.jpg" alt="Thirteen slide">
+                                    <img class="d-block img-fluid"
+                                         src="./images/qutub%20minar%20slice%20of%20history.jpg"
+                                         alt="Thirteen slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/qutub%20minar%202.png" alt="Fourteenth slide">
+                                    <img class="d-block img-fluid" src="./images/qutub%20minar%202.png"
+                                         alt="Fourteenth slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block img-fluid" src="./images/surajkundm_mela2.jpg" alt="Fifteenth slide">
+                                    <img class="d-block img-fluid" src="./images/surajkundm_mela2.jpg"
+                                         alt="Fifteenth slide">
                                 </div>
                             </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                               data-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
                             </a>
-                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                               data-slide="next">
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                             </a>
